5829287a1bef3d6471f6bc57539b86608e497e11
Zakia Ben Youss <z.benyouss@gmail.com>
2020-10-24 12:07:37 -0700

    Add files via upload

---
+++ Calling_Name_Function.py
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Basic Python Script: This is a simple Script that calles function name() from main code. 
+# Zakia Ben Youss Gironda
+
+name ()
+{
+echo "Zakia"
+return
+}
+
+
+#Main code starts here
+
+echo "This code is written by"
+name
+
+
+
